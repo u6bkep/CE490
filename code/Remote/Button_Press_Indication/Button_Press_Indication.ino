a17e27a17f3703a9b1bca90d07a11f1502327123
Babymonty10 <boltonhayvin@hotmail.com>
2022-01-28 16:49:00 -0500

    Code for the remote Controller
    Established wireless communication between boards, code to check if buttons work, and code to obtain mac address of boards.

---
+++ code/Remote/Button_Press_Indication/Button_Press_Indication.ino
@@ -0,0 +1,28 @@
+// Button
+
+  #define BLUEBUTTON_PIN 5  // GIOP21 pin connected to button
+
+// Variables will change:
+int lastState = LOW;  // the previous state from the input pin
+int currentState;     // the current reading from the input pin
+
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  // initialize the pushbutton pin as an pull-up input
+  // the pull-up input pin will be HIGH when the switch is open and LOW when the switch is closed.
+  pinMode(BLUEBUTTON_PIN, INPUT_PULLUP);
+}
+
+void loop() {
+  // read the state of the switch/button:
+  currentState = digitalRead(BLUEBUTTON_PIN);
+
+  if (lastState == HIGH && currentState == LOW)
+    Serial.println("The button is pressed");
+  else if (lastState == LOW && currentState == HIGH)
+    Serial.println("The button is released");
+
+  // save the the last state
+  lastState = currentState;
+}
