1d63a2a492138d5cc24c81ba474da495f27b3e23
Akkurai <98362217+Akkurai@users.noreply.github.com>
2022-02-18 16:22:14 -0500

    motor code

--- code/Motor/Motor.ino
+++ code/Motor/Motor.ino
@@ -2,9 +2,9 @@
 
 //Required Libraries Importation
 #include <esp_now.h>
-#include <WiFi.h>
+//#include <WiFi.h>
 
-#define motor_pin 34; //temporary
+#define motor_pin 5 //temporary
 
 // Structure example to receive data
 // Must match the sender structure
@@ -14,7 +14,7 @@ typedef struct buttonState_set {
   bool motorDown = 0;  // 0 for not pressed, 1 for pressed
   bool nextImg = 0;    // 0 for not pressed, 1 for pressed, motor does not care
   bool prevImg = 0;    // 0 for not pressed, 1 for pressed, motor does not care
-} struct_message;
+} buttonState_struct;
 
 // Create a struct_message called myData
 buttonState_struct myData;
@@ -24,33 +24,37 @@ int motorSpeed = 0; //lowest speed is 0, max is 5?
 // callback function that will be executed when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&myData, incomingData, sizeof(myData));
-  if (MyData.power) {
+  if (myData.power) {
     //the power button was pressed
     if (!onOff)
     {
       //turn the motor on
-      maxSpeed = 1;
-      analogWrite(motor_pin, maxSpeed);
+      motorSpeed = 1;
+      analogWrite(motor_pin, motorSpeed);
     }
     else {
       //turn the motor off
-      maxSpeed = 0;
-      analogWrite(motor_pin, maxSpeed);
+      motorSpeed = 0;
+      analogWrite(motor_pin, motorSpeed);
     }
 
   }
-  else if (MyData.MotorUp) {
+  else if (myData.MotorUp) 
+  {
     //increase the motor speed
-    if (maxSpeed < 5) {
-      maxSpeed = maxSpeed + 1;
-      analogWrite(motor_pin, maxSpeed);
+    if (motorSpeed < 5) 
+    {
+      motorSpeed = motorSpeed + 1;
+      analogWrite(motor_pin, motorSpeed);
     }
   }
-  else if (MyData.MotorDown) {
+  else if (myData.MotorDown)
+  {
     //decrease the motor speed
-    if (maxSpeed > 0) {
-      maxSpeed = maxSpeed - 1;
-      analogWrite(motor_pin, maxSpeed);
+    if (motorSpeed > 0) 
+    {
+      //motorSpeed = motorSpeed - 1;
+      //analogWrite(motor_pin, motorSpeed);
     }
   }
 }
@@ -59,10 +63,10 @@ void setup() {
   // put your setup code here, to run once:
 
   pinMode (motor_pin, OUTPUT); //set the motor pin as an output
-  
+
   // Initialize Serial Monitor
   Serial.begin(115200);
-  
+
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
@@ -71,7 +75,7 @@ void setup() {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
-  
+
   // Once ESPNow is successfully Init, we will register for recv CB to
   // get recv packer info
   esp_now_register_recv_cb(OnDataRecv);
